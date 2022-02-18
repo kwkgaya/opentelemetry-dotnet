6b0b9c9a7f99d09ddf51631e3b4d51e455f5379b
Kavinda Gayashan <Kavinda@CreativeSoftware.com>
2022-02-18 16:18:36 +0530

    Fix build

--- src/OpenTelemetry.Exporter.OpenTelemetryProtocol.Logs/OtlpLogExporterHelperExtensions.cs
+++ src/OpenTelemetry.Exporter.OpenTelemetryProtocol.Logs/OtlpLogExporterHelperExtensions.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // </copyright>
-#if !XAMARIN
+
 using System;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Internal;
@@ -59,4 +59,3 @@ namespace OpenTelemetry.Logs
         }
     }
 }
-#endif
